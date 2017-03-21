baa68006842427468b5d9a2b4400cd9398890b7d
iwax2 <iwaiwa2mail@gmail.com>
2017-03-21 10:24:28 +0900

    読み込み書き込みをなんとなく作った

--- armadillo_lpr9204.py
+++ armadillo_lpr9204.py
@@ -2,7 +2,23 @@
 # -*- coding: utf-8 -*-
 
 # Armadillo上でLPR9204から送られてきたデータを受信してCSV保存するスクリプト
+import serial
+import sys
 
+f = open('temperature.csv', 'w')
+with serial.Serial('/dev/ttymxc1', 115200, timeout=1) as s:
 
+try:
+    while True:
+        line = s.readline()   # read a '\n' terminated line
+        f.write(line)
+except KeyboardInterrupt:
+    print("W: interrupt received, stopping script")
+finally:
+    print("W: close…")
+    s.close()
+    f.close()
+
+sys.exit()
 
 
