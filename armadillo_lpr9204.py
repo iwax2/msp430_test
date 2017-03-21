db98fba047ed7c7dd4569b9ed73a9da9c439d0a5
iwax2 <iwaiwa2mail@gmail.com>
2017-03-21 14:32:57 +0900

    時刻、ID、温度、湿度をCSVに保存した

--- armadillo_lpr9204.py
+++ armadillo_lpr9204.py
@@ -4,18 +4,29 @@
 # Armadillo上でLPR9204から送られてきたデータを受信してCSV保存するスクリプト
 import serial
 import sys
+from datetime import datetime
 
 f = open('temperature.csv', 'w')
-with serial.Serial('/dev/ttymxc1', 115200, timeout=1) as s:
+s = serial.Serial('/dev/ttymxc1', 115200, timeout=1)
 
 try:
     while True:
-        line = s.readline()   # read a '\n' terminated line
-        f.write(line)
+        line = s.readline().decode('utf-8')
+        if( line.startswith('E') ):
+            data = line.split()
+            if( len(data)>=8 and data[0] == 'ERXDATA' ):
+                date = datetime.now().strftime("%Y/%m/%d,%H:%M:%S")
+                dest = data[1]
+                t_h = data[7].split(',')
+                temp = t_h[0]
+                humi = t_h[1]
+                csv = date+','+dest+','+temp+','+humi
+                print(csv)
+                f.write(csv+'\r\n')
 except KeyboardInterrupt:
     print("W: interrupt received, stopping script")
 finally:
-    print("W: close…")
+    print("W: close...")
     s.close()
     f.close()
 
