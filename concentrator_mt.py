61e076648cde38899d54d403941ed5891dcd2dbc
Hiroshi Iwata <iwaiwa2mail@gmail.com>
2017-04-04 23:44:33 +0900

    ACKを見て再送制御できるようにした

--- concentrator_mt.py
+++ concentrator_mt.py
@@ -22,6 +22,9 @@ NODE_LIST_FILE = 'node_list.txt'
 received_packets = deque() # パケット保存用キュー
 accepted_packets = [] # 受理したパケット保存用リスト
 coordinate = {} # 各ノードの座標保管用ディクショナリ (16進4桁ノードID, x座標, y座標)
+ack_message_queue = deque() # EACKメッセージ保存用キュー
+message_id_queue = deque()  # 920が割り振るメッセージID保存用キュー
+error_code_queue = deque()  # FAIL ER10などのエラーコード保存用キュー
 
 s = serial.Serial('/dev/ttymxc1', 115200, timeout=1)
 packet_number = 0
@@ -33,20 +36,25 @@ packet_number = 0
 '''
 def receive_packet(e):
     while not e.isSet():
-        s.flush()
         if( s.inWaiting() > 0 ):
             line = s.readline().decode('utf-8').rstrip()
-            print("    Received: "+ line)
-            if( line.startswith('E') ):
-                data = line.split()
-                if( len(data)>=8 and data[0] == 'ERXDATA' ):
-                    send_id = data[1]
-                    t_and_h = data[7].split(',')
-                    packet_number = int(t_and_h[0]) # packet_id
-                    temp    = t_and_h[1]
-                    humi    = t_and_h[2]
-                    packet  = (send_id, packet_number, temp, humi)
-                    received_packets.append(packet)
+            print("    920 Says>"+ line)
+            data = line.split()
+            if( len(data)>=8 and data[0] == 'ERXDATA' ):
+                send_id = data[1]
+                t_and_h = data[7].split(',')
+                packet_number = int(t_and_h[0]) # packet_id
+                temp    = t_and_h[1]
+                humi    = t_and_h[2]
+                packet  = (send_id, packet_number, temp, humi)
+                received_packets.append(packet)
+            elif( len(data)>=4 and data[0] == 'EACK' ):
+                ack_result = ( data[1], data[3] ) # STATUS, MSG_ID
+                ack_message_queue.append( ack_result )
+            elif( len(data)>=2 and data[1] == 'OK' ):
+                message_id_queue.append(data[0])
+            elif( len(data)>=2 and data[0] == 'FAIL' ):
+                error_code_queue.append(data[1])
 
 '''
  受け取ったパケットを処理する(ARQ)スレッド
@@ -55,7 +63,7 @@ def receive_packet(e):
  2. 送られてきていないノードに対しては再送要求
  3. データが揃えばスレッド終了
 '''
-def automatic_repeat_request(packet_number):
+def automatic_repeat_request(e, packet_number):
     coordinate.clear()
     with codecs.open( NODE_LIST_FILE, 'r', 'utf-8' ) as f:
         for line in f:
@@ -65,9 +73,9 @@ def automatic_repeat_request(packet_number):
                 data = comment[0].split(',')
                 coordinate[data[0]] = (data[1], data[2])
                 print(data)
-    accepted_node_list = []
+    accepted_node_list = [] # 対象のパケット番号で受理したパケットの管理用
     start_sec = datetime.now().second
-    while( len(accepted_packets) < len(coordinate) ):
+    while( len(accepted_packets) < len(coordinate) and not e.isSet() ):
         if( len(received_packets)>0 ):
             packet = received_packets.popleft()
             if( packet[1] == packet_number ):
@@ -81,39 +89,70 @@ def automatic_repeat_request(packet_number):
                 start_sec = datetime.now().second # 前回の実行から10秒以上経っていれば再送要求する
                 for node in coordinate:
                     if node not in accepted_node_list:
-                        s.flush()
-                        cmd = "SKSEND 1 1000 0002 0C RSEND,"+node+","+str(packet_number)+"\r\n"
-                        print(cmd)
-                        s.write(cmd.encode('utf-8'))
-                        sleep(2)
-                        s.flush()
-                        print(cmd)
-                        s.write(cmd.encode('utf-8'))
+                        send_packet("SKSEND 1 1000 0002 0C RSEND,"+node+","+str(packet_number))
+
+'''
+ パケット送信（再送制御つき）
+ メソッド側で改行コードは面倒を見ます
+'''
+def send_packet( command ):
+    serial_command = command + "\r\n"
+    s.flush()
+    ack_message_queue.clear()
+    message_id_queue.clear()
+    error_code_queue.clear()
+    print(command)
+    s.write(serial_command.encode('utf-8'))
+    message_id = "" # ランダムの4桁16進値。SKSENDを発行したらMSG_ID OKの形で返ってくる（ハズ
+    while True:
+        if( len(message_id_queue) > 0 ):
+            message_id = message_id_queue.popleft()
+            break
+    while True:
+        if( len(ack_message_queue) > 0 ):
+            if( pop_ack_message( message_id ) == '1' ):
+                break
+            else:
+                print("[Resend because of  nack] "+command)
+                s.flush()
+                s.write(serial_command.encode('utf-8'))
+        elif( len(error_code_queue) > 0 ): # エラーメッセージがでたときは再送
+            print("[Resend because of error] "+command)
+            s.flush()
+            s.write(serial_command.encode('utf-8'))
+
+'''
+ ack_message_queueから対象のmessage_idを含むACK結果を取り出して返します
+'''
+def pop_ack_message( message_id ):
+    for ack in ack_message_queue: # ack[0] = STATUS, ack[1] = MSG_ID
+        if( ack[1] == message_id ):
+            ack_message_queue.remove(ack)
+            return ack[0] # ack_status '0' or '1'
+    return '0' # ACKが受け取れていないので'0'を返して再送
+
 
 '''
  メインスレッド
 '''
-e = threading.Event()
-th_rec = threading.Thread(name='rec', target=receive_packet, args=(e,))
+e_rec = threading.Event()
+e_arq = threading.Event()
+th_rec = threading.Thread(name='rec', target=receive_packet, args=(e_rec,))
 th_rec.start()
 try:
     while True:
-        th_arq = threading.Thread(name='arq', target=automatic_repeat_request, args=(packet_number,))
+        e_arq.clear()
+        th_arq = threading.Thread(name='arq', target=automatic_repeat_request, args=(e_arq, packet_number))
         th_arq.start()
         th_arq.join(30) # 30秒で打ち切る
+        e_arq.set()
         packet_number = ( packet_number+1 ) % 10
         sleep_time = 60 - datetime.now().second
-        cmd = "SKSEND 1 1000 0002 0F SLEEP_ALL_,"+str(packet_number)+","+str(sleep_time)+"\r\n"
-        s.flush()
-        s.write(cmd.encode('utf-8'))
-        print(cmd)
-        sleep(5)
-        s.flush()
-        s.write(cmd.encode('utf-8'))
-        print(cmd)
+        # SLEEPを送るのはブロードキャストになると思うので別メソッドになる（予定）
+        send_packet("SKSEND 1 1000 0002 0F SLEEP_ALL_,"+str(packet_number)+","+str(sleep_time))
         date = datetime.now().strftime("%Y/%m/%d,%H:%M:%S")
         for packet in accepted_packets:
-            csv = date+','+','+packet[2]+','+packet[3]
+            csv = date+','+','+packet[2]+','+packet[3] # packet = (send_id, packet_number, temp, humi)
             with open('temperature_'+ packet[0] +'.csv','a') as f:
                 f.write(csv+'\r\n')
         accepted_packets.clear()
