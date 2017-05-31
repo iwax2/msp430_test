f69ad0c59e671c082d952d3c3933341a3e72d275
iwax2 <iwaiwa2mail@gmail.com>
2017-05-31 14:23:37 +0900

    湿度

--- ColorMap/ColorMap.py
+++ ColorMap/ColorMap.py
@@ -13,7 +13,8 @@ from scipy.interpolate import Rbf # RBF補間
 import matplotlib.cm as cm # カラーバーの設定
 
 # 参考 http://www.mwsoft.jp/programming/numpy/csv.html
-data = np.loadtxt("assmann_20170302.txt", delimiter="\t", dtype=np.float64, usecols=(0,1,2)) # , filling_values=(0, 0, 0) 
+data = np.loadtxt("html_20170529.txt", delimiter="\t", dtype=np.float64, usecols=(0,1,3)) # , filling_values=(0, 0, 0) 
+#data = np.loadtxt("assmann_20170302.txt", delimiter="\t", dtype=np.float64, usecols=(0,1,2)) # , filling_values=(0, 0, 0) 
 #data = np.loadtxt("windspeed_test.txt", delimiter="\t", dtype=np.float64, usecols=(0,1,2)) # , filling_values=(0, 0, 0) 
 
 # 参考 http://www.mwsoft.jp/programming/numpy/csv.html
@@ -100,8 +101,8 @@ z = np.append(z,za)
 
 print(x)
 '''
-x,y = np.meshgrid(np.arange(0, 10, 0.1), np.arange(0, 10, 0.1))
-z = 2*np.random.randn(100,100)+20
+#x,y = np.meshgrid(np.arange(0, 10, 0.1), np.arange(0, 10, 0.1))
+#z = 2*np.random.randn(100,100)+20
 
 #x=np.array([[1,1,1],[6,6,6],[8,8,8],[10,10,10],[15,15,15]])
 #y=np.array([[1,5,10],[1,5,10],[1,5,10],[1,5,10],[1,5,10]])
@@ -110,11 +111,11 @@ z = 2*np.random.randn(100,100)+20
 # 2D補間
 # https://docs.scipy.org/doc/scipy/reference/tutorial/interpolate.html#id1
 XI, YI = np.meshgrid(np.arange(0, 15.1, 0.1), np.arange(0, 10.1, 0.1))
-#rbf = Rbf(x, y, z, function='multiquadric', epsilon=9.0, smooth=0)
+rbf = Rbf(x, y, z, function='multiquadric', epsilon=9.0, smooth=0)
 #rbf = Rbf(x, y, z, epsilon=2)
-#ZI = rbf(XI, YI)
-tck = interpolate.bisplrep(x, y, z, s=0)
-ZI= interpolate.bisplev(XI[:,0], YI[0,:], tck)
+ZI = rbf(XI, YI)
+#tck = interpolate.bisplrep(x, y, z, s=0)
+#ZI= interpolate.bisplev(XI[:,0], YI[0,:], tck)
 plt.pcolor(XI, YI, ZI, cmap=cm.jet)
 #plt.pcolor(x, y, z, cmap=cm.jet)
 #plt.scatter(x, y, 100, z, cmap=cm.jet, linewidths=1, edgecolors="black")
@@ -124,7 +125,8 @@ print(ZI)
 
 
 # 参考 http://seesaawiki.jp/met-python/d/matplotlib
-plt.title("Temperature [${}^\circ$C]", fontsize=16)
+#plt.title("Temperature [${}^\circ$C]", fontsize=16)
+plt.title("Humidity [%]", fontsize=16)
 plt.axis([1,15,1,10])
 #plt.locator_params(axis='both', tight=True, nbins=10)
 plt.colorbar()
